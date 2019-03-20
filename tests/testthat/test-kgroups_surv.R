46b36945304babf8dc8eea6096cd8ba4efa759ea
Nora M. Villanueva <noramvillanueva@gmail.com>
2019-03-20 19:45:06 +0100

    fixed unit test

--- tests/testthat/test-kgroups_surv.R
+++ tests/testthat/test-kgroups_surv.R
@@ -8,7 +8,7 @@ data(veteran)
 test_that(
   "Output is correct in kroups_surv function",
   {
-    expected <- c("squamous", "smallcell", "adeno", "large" )
+    #expected <- c("squamous", "smallcell", "adeno", "large" )
     fit <- kgroups_surv(time = veteran$time, status = veteran$status,
                           fac = veteran$celltype, k = 2, algorithm = "kmeans",
                           seed = 300716)
